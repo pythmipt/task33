b2bec11f847d5870b70a3da9f17380530ed76fa1
maximBalandin2009 <131290554+maximBalandin2009@users.noreply.github.com>
2023-04-26 19:57:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ print("Объем куба равен ", math_func.cube(a))
 a, b = input("Укажите катеты прямоуг. треуг. через пробел ").strip().split()
 print("Гипотенуза ", math_func.hipot(float(a), float(b)))
 
-
+print('я изменил')
 
 
